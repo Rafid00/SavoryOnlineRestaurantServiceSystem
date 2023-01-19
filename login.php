14210a5c3da9805817b8128110ff48cd9026193f
Rafid00 <rafid.ahmmad.3@gmail.com>
2023-01-20 02:00:35 +0600

    login page creation

--- login.php
+++ login.php
@@ -1,17 +1,3 @@
-<?php
-session_start();
-
-//   if (!isset($_SESSION['name'])) {
-//   	$_SESSION['msg'] = "You must log in first";
-//   	header('location: login.php');
-//   }
-if (isset($_GET['logout'])) {
-    session_destroy();
-    unset($_SESSION['name']);
-    header("location: index.php");
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -102,85 +88,30 @@ if (isset($_GET['logout'])) {
     </style>
 </head>
 
-<body class="bg-[#282421] overflow-x-hidden">
-
+<body class="bg-[#E8C07D] overflow-x-hidden">
     <?php require "nav-component.php" ?>
-
     <div class="scroll-container h-screen w-screen flex flex-col">
-        <section class="scroll-child w-screen h-screen">
-            <div class="login">
-                <div class="left-side-login">
-                    
+        <section class="scroll-child h-screen bg-[#282421] w-screen relative overflow-hidden">
+            <div class="bg-img">
+                <div class="main-filter h-screen bg-black opacity-60 w-screen absolute top-0 left-0 z-10"></div>
+                <img class="absolute bottom-0 object-cover" src="images/login-bg.jpg">
+            </div>
+            <div class="flex shadow-md login-form z-20 w-[1100px] h-[650px] bg-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded">
+                <div class="left-part-login bg-[#282421] w-[35%] text-white">
+                    <h1>NEW HERE?</h1>
+                    <p>Register now to discover our food items and unlock our services.</p>
                 </div>
-
-                <div class="right-side-login">
+                <div class="right-part-login">
                     
                 </div>
             </div>
         </section>
 
-        <footer
-            class="footer scroll-child w-screen h-[50vh] text-white z-50 border-t-[1px] border-t-neutral-500 bg-[#282421] relative">
-            <div class="footer-containter absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-                <div class="flex justify-between items-start w-[80vw]">
-                    <div class="footer-left">
-                        <div class="title text-white text-6xl z-50 mb-5 text-center border-2 w-fit">Khanas</div>
-                        <div class="nav-links-footer">
-                            <p class="w-[250px] mb-7">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero soluta illum voluptas
-                                fugit
-                                repudiandae dicta
-                                architecto temporibus est delectus aperiam.
-                            </p>
-                            <div class="social-footer flex justify-between items-center w-[220px]">
-                                <a href=""><img class="w-[30px] my-2 hover:scale-110 transition-all duration-75"
-                                        src="images/icons8-facebook.svg" alt="" /></a>
-                                <a href=""><img class="w-[30px] my-2 hover:scale-110 transition-all duration-75"
-                                        src="images/icons8-instagram.svg" alt="" /></a>
-                                <a href=""><img class="w-[30px] my-2 hover:scale-110 transition-all duration-75"
-                                        src="images/icons8-twitter.svg" alt="" /></a>
-                                <a href=""><img class="w-[30px] my-2 hover:scale-110 transition-all duration-75"
-                                        src="images/icons8-github.svg" alt="" /></a>
-                                <a href=""><img class="w-[30px] my-2 hover:scale-110 transition-all duration-75"
-                                        src="images/icons8-youtube.svg" alt="" /></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="footer-center">
-                        <div class="nav-links-footer">
-                            <div class="text-white text-2xl z-50 w-fit font-medium">Navigations</div>
-                            <div class="h-[2px] w-[100px] bg-white my-4 rounded"></div>
-
-                            <ul class="flex flex-col gap-2 justify-center">
-                                <li><a href="index.php">HOME</a></li>
-                                <li><a href="about-page.php">ABOUT</a></li>
-                                <li><a href="contact-us.php">CONTACT</a></li>
-                                <li><a href="profile-page.php">PROFILE</a></li>
-                                <li><a href="">ORDER</a></li>
-                                <li><a href="">FEEDBACK</a></li>
-                                <li><a href="">RESERVATION</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="footer-right">
-                        <div class="text-white text-2xl z-50 w-fit font-medium">Location</div>
-                        <div class="h-[2px] w-[100px] bg-white my-4 rounded"></div>
-
-                        <div class="nav-links-footer">
-                            <p class="w-[250px] mb-10">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                                Quisquam, rem?
-                            </p>
-                            <p class="my-2"><span class="underline">Phone:</span> +8801423542132</p>
-                            <p class="my-2"><span class="underline">Email:</span> <a
-                                    href="">rafid.ahmmad.3@gmail.com</a></p>
-                            <p class="my-2"><span class="underline">Website:</span> <a href="">www.randomwebsite.com</a>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <div class="scroll-child">
+            <?php require "footer.php" ?>
+        </div>
     </div>
+
 </body>
 
 </html>
