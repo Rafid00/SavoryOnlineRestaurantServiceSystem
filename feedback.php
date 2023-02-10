8ea643ae122a5a575182801a3714cdf9dd07494c
Rafid00 <rafid.ahmmad.3@gmail.com>
2023-02-10 23:48:11 +0600

    feedback page under construction

--- feedback.php
+++ feedback.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['name']);
+    header("location: index.php");
+}
+?>
+
+<?php
+if (!isset($_SESSION['id'])) {
+    header('location: login.php');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -88,16 +104,16 @@
     </style>
 </head>
 
-<body class="bg-[#E8C07D] overflow-x-hidden">
+<body class="bg-[#282421] overflow-x-hidden">
     <?php require "nav-component.php" ?>
     <div class="h-screen w-screen flex flex-col">
-        <section class="h-screen bg-[#282421] w-screen relative grid grid-cols-3" id="feedback">
-            <div class="reviews col-span-2">
-
+        <section class="h-screen bg-[#282421] relative flex flex-wrap justify-center pt-[100px] w-[90%] mx-auto"
+            id="feedback">
+            <div class="reviews grid grid-cols-2 h-screen flex-grow">
             </div>
-            <div class="review-form">
+            <div class="review-form p-10 rounded-md flex flex-col justify-center items-start w-fit h-fit" style="box-shadow: -1px 2px 18px 3px rgba(0,0,0,0.35); -webkit-box-shadow: -1px 2px 18px 3px rgba(0,0,0,0.35); -moz-box-shadow: -1px 2px 18px 3px rgba(0,0,0,0.35);">
+                <h2 class="text-white uppercase text-2xl font-semibold mb-10">Write A Review</h2>
                 <?php
-
                 include("config.php");
 
                 if (isset($_SESSION['id'])) {
@@ -113,16 +129,78 @@
                     $image = $row['image_name'];
 
                 }
-
                 ?>
 
-                <div class="relative">
-                    <textarea id="message" name="message"
-                        class="block px-3 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                <div class="flex justify-between items-center w-full mb-5">
+                    <div class="pic-name-profile flex justify-start items-center gap-4">
+                        <div class="profile-pic w-[65px]">
+                            <img class="rounded-full object-cover object-top" src="<?php echo $image; ?>" alt="" />
+                        </div>
+                        <div class="profile-name text-white">
+                            <div class="flex items-center">
+                                <p class="text-lg border-white w-fit">
+                                    <?php echo ucwords($name); ?>
+                                </p>
+                            </div>
+                            <p class="">
+                                <span class="text-blue-500">
+                                    <a href="">
+                                        <?php echo $email; ?>
+                                    </a>
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="flex items-center px-5 py-2 rounded-lg bg-zinc-700">
+                        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <title>First star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                            </path>
+                        </svg>
+                        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <title>Second star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                            </path>
+                        </svg>
+                        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <title>Third star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                            </path>
+                        </svg>
+                        <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <title>Fourth star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                            </path>
+                        </svg>
+                        <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor"
+                            viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <title>Fifth star</title>
+                            <path
+                                d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                            </path>
+                        </svg>
+                    </div>
+                </div>
+
+                <div class="relative mb-10">
+                    <textarea id="message" name="message" rows="3" cols="50"
+                        class="block px-3 pb-2.5 pt-4 text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                         placeholder=" " autocomplete="off" required /></textarea>
                     <label for="message"
                         class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-[#282421] px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Message</label>
                 </div>
+
+                <button type="submit" class="text-white py-2 px-7 bg-green-600 rounded-lg ml-auto">Submit</button>
+
             </div>
         </section>
 
