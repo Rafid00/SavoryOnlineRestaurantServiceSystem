db7633be2bc822d3e28b1d1c986ebeeb92e12124
Rafid00 <rafid.ahmmad.3@gmail.com>
2023-05-25 23:05:29 +0600

    database updated

--- savory.sql
+++ savory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 12:02 AM
+-- Generation Time: May 25, 2023 at 07:05 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
@@ -34,6 +34,20 @@ CREATE TABLE `cart` (
   `quantity` tinyint(4) NOT NULL DEFAULT 0,
   `total_price` float NOT NULL,
   `added_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `feedback_id` varchar(36) NOT NULL,
+  `user_id` varchar(36) NOT NULL,
+  `rating` tinyint(4) NOT NULL,
+  `message` varchar(300) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -77,14 +91,6 @@ CREATE TABLE `messages` (
   `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `messages`
---
-
-INSERT INTO `messages` (`user_id`, `name`, `email`, `message`) VALUES
-('88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'Rafid', 'Rafid@gmail.com', 'Helol!'),
-('88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'Rafid', 'rafid@gmail.com', 'Helol Again!');
-
 -- --------------------------------------------------------
 
 --
@@ -101,19 +107,6 @@ CREATE TABLE `transaction_info` (
   `delivery_address` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `transaction_info`
---
-
-INSERT INTO `transaction_info` (`transaction_id`, `customer_id`, `order_type`, `date`, `payment_method`, `amount`, `delivery_address`) VALUES
-('301201c5-81af-11ed-922e-7c10c9a046bd', '88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'delivery', '2022-12-22 10:14:54', 'cash', 15.38, 'pallabi, mirpur, dhaka - 1216'),
-('3e6adf15-e159-11ed-acad-7c10c9a046bd', '88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'pre-order', '2023-04-23 04:01:33', 'cash', 26.61, NULL),
-('3f58a390-baa5-11ed-8fcf-7c10c9a046bd', '88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'pre-order', '2023-03-04 21:57:21', 'cash', 6.82, NULL),
-('5c018b6c-baa5-11ed-8fcf-7c10c9a046bd', '88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'pre-order', '2023-03-04 21:58:09', 'cash', 12.17, NULL),
-('8f91926f-d719-11ed-901a-7c10c9a046bd', '69e259ff-d719-11ed-901a-7c10c9a046bd', 'pre-order', '2023-04-10 03:00:30', 'cash', 12.7, NULL),
-('a47411a9-81af-11ed-922e-7c10c9a046bd', '88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'pre-order', '2022-12-22 10:18:09', 'cash', 30.89, NULL),
-('f51493db-81ae-11ed-922e-7c10c9a046bd', '88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'pre-order', '2022-12-22 10:13:15', 'cash', 11.63, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -125,32 +118,6 @@ CREATE TABLE `transaction_items` (
   `item_name` text NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `transaction_items`
---
-
-INSERT INTO `transaction_items` (`transaction_id`, `item_name`, `quantity`) VALUES
-('d81a429b-81ae-11ed-922e-7c10c9a046bd', 'Strawberry Coffee', 2),
-('d81a429b-81ae-11ed-922e-7c10c9a046bd', 'Quarter Pounder', 2),
-('f51493db-81ae-11ed-922e-7c10c9a046bd', 'Quarter Pounder', 1),
-('f51493db-81ae-11ed-922e-7c10c9a046bd', 'Rafid Cheese Pizza', 2),
-('0ce75290-81af-11ed-922e-7c10c9a046bd', 'Special Cold Coffee', 1),
-('0ce75290-81af-11ed-922e-7c10c9a046bd', 'Rafid Cheese Pizza', 4),
-('301201c5-81af-11ed-922e-7c10c9a046bd', 'Quarter Pounder', 1),
-('301201c5-81af-11ed-922e-7c10c9a046bd', 'Rafid Cheese Pizza', 3),
-('301201c5-81af-11ed-922e-7c10c9a046bd', 'Special Cold Coffee', 1),
-('a47411a9-81af-11ed-922e-7c10c9a046bd', 'Quarter Pounder', 9),
-('3f58a390-baa5-11ed-8fcf-7c10c9a046bd', 'Special Cold Coffee', 3),
-('3f58a390-baa5-11ed-8fcf-7c10c9a046bd', 'Rafid Cheese Pizza', 1),
-('5c018b6c-baa5-11ed-8fcf-7c10c9a046bd', 'Rafid Cheese Pizza', 2),
-('5c018b6c-baa5-11ed-8fcf-7c10c9a046bd', 'Special Cold Coffee', 1),
-('5c018b6c-baa5-11ed-8fcf-7c10c9a046bd', 'Quarter Pounder', 1),
-('8f91926f-d719-11ed-901a-7c10c9a046bd', 'Strawberry Coffee', 1),
-('8f91926f-d719-11ed-901a-7c10c9a046bd', 'Quarter Pounder', 3),
-('3e6adf15-e159-11ed-acad-7c10c9a046bd', 'Strawberry Coffee', 2),
-('3e6adf15-e159-11ed-acad-7c10c9a046bd', 'Quarter Pounder', 2),
-('3e6adf15-e159-11ed-acad-7c10c9a046bd', 'Rafid Cheese Pizza', 5);
 
 -- --------------------------------------------------------
 
@@ -173,12 +140,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `phone`, `email`, `password`, `bio`, `image_name`) VALUES
-('6326e216-a87f-11ed-b680-7c10c9a046bd', 'raf', '123456', 'raf@gmail.com', 'e205a719aa840c828789590b5eb3b62d', 'No Biography Added', 'images/uploads/propic.jpg'),
-('69e259ff-d719-11ed-901a-7c10c9a046bd', 'rafid', '123456', 'raf1@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'No Biography Added', 'images/uploads/339451501_757866749280987_6220166980426963357_n.jpg'),
-('7cfce3f4-a87e-11ed-b680-7c10c9a046bd', 'rafid', '12345', 'rafid2@gmail.com', '39de7ab7f9cefe9a6530bb5076d0bf6c', 'No Biography Added', 'images/uploads/propic.jpg'),
-('88d6049d-8103-11ed-8a9a-7c10c9a046bd', 'rafid', '12345', 'rafid@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Hi, I am Rafid.', 'images/uploads/NeGaTivE_remove_texts_ultra_hd_realistic_batman_07bd380d-5762-4094-a008-fbe887e2512d.png'),
-('cef0607f-a3aa-11ed-a95c-7c10c9a046bd', 'rafid ahmmad', '12345', 'rafid1@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'Hi, Everyone. I am Elong Maa.', 'images/uploads/PXL_20230107_152610125.PORTRAIT-02.jpeg'),
-('f3e098f1-aa1f-11ed-a65d-7c10c9a046bd', 'rafid1@gmail.com', '1234567', 'rafid12@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'No Biography Added', 'images/uploads/propic.jpg');
+('daa7f95b-fb1d-11ed-bde9-7c10c9a046bd', 'rafid ahmmad', '1234567', 'rafid@gmail.com', '25f9e794323b453885f5181f1b624d0b', 'No Biography Added', 'images/uploads/propic.jpg');
 
 --
 -- Indexes for dumped tables
@@ -190,6 +152,12 @@ INSERT INTO `users` (`id`, `name`, `phone`, `email`, `password`, `bio`, `image_n
 ALTER TABLE `cart`
   ADD KEY `customer_id` (`customer_id`),
   ADD KEY `item_name` (`item_name`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`feedback_id`);
 
 --
 -- Indexes for table `food_items`
